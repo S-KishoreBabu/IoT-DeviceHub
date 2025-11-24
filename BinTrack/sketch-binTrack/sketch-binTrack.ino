65d2b936464657db0a0c65a661b42d237b51d413
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-11-24 15:12:03 +0530

    Refactor WiFi connection logic and update Firebase configuration

--- BinTrack/sketch-binTrack/sketch-binTrack.ino
+++ BinTrack/sketch-binTrack/sketch-binTrack.ino
@@ -1,10 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
 
-#define WIFI_SSID "POCO F5"
-#define WIFI_PASSWORD "123456789"
+#define WIFI_SSID "Sanjiv"
+#define WIFI_PASSWORD "123123123"
 
-// Firebase config
 #define FIREBASE_HOST "brave-design-417105-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "AIzaSyCzi2IsDsZE5WIA7KpDgS-0RFs6vxSDqDY"
 
@@ -12,32 +11,40 @@
 #define ECHO_PIN D6  
 
 FirebaseData firebaseData;
-FirebaseAuth auth;
-FirebaseConfig config;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+void connectToWiFi() {
   Serial.print("Connecting to WiFi");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nWiFi connected");
 
-  
-  config.host = FIREBASE_HOST;
-  config.signer.tokens.legacy_token = FIREBASE_AUTH;
+  Serial.println("\nWiFi Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
 
-  Firebase.begin(&config, &auth);
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  connectToWiFi();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi Disconnected! Reconnecting...");
+    connectToWiFi();
+  }
+
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -52,11 +59,12 @@ void loop() {
   Serial.println(" cm");
 
   bool isFilled = distance <= 15;
+
   if (Firebase.setBool(firebaseData, "/dustbins/001/filled", isFilled)) {
-    Serial.println("Updated to Firebase: " + String(isFilled));
+    Serial.println("Firebase update OK");
   } else {
-    Serial.println("Firebase update failed: " + firebaseData.errorReason());
+    Serial.println("Firebase update FAILED: " + firebaseData.errorReason());
   }
 
-  delay(5000);
+  delay(5000);
 }
